679152e65305e8cdbf7a5306a0c53c5d56241e4c
tharakadasun <126054222+tharakadasun@users.noreply.github.com>
2023-03-30 10:40:19 +0530

    Add files via upload

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,10 +1,43 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { createOrder } from "../../redux/order/OrderSlice";
 import SmallCard from "../card/SmallCard";
 
 function Cart() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { items } = useSelector((state) => state.cart);
+
+
+
+  const handlePlaceOrder = () =>{
+    const local_token = localStorage.getItem('token')
+    console.log('token:- ',local_token);
+    let cart_items = [];
+    items.forEach(element => {
+      cart_items.push({
+        productId: element.id,
+        quantity:1
+      })
+    });
+    const cartItemBody = {
+      orderItems: cart_items
+    }
+    if(local_token != null){
+      if(items != null){
+        console.log("c");
+        dispatch(createOrder(cartItemBody))
+        navigate("/checkout")
+      }else{
+        toast.error("Please add items")
+      }
+    }else{
+      toast.error("Please Login !")
+    }
+  }
+
   console.log(items);
   return (
     <>
@@ -36,7 +69,7 @@ function Cart() {
             <></>
           )}
           <div className="d-flex flex-row">
-              <button type="button" className="btn btn-primary flex-fill" data-mdb-ripple-color="dark">
+              <button aria-label="Close" data-bs-dismiss="offcanvas" onClick={handlePlaceOrder} type="button" className="btn btn-primary flex-fill" data-mdb-ripple-color="dark">
                 Place Order
               </button>
             </div>
