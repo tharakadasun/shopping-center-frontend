48e95897adb3e94f79bd32fc031303eabe1d3f1a
tharakadasun <126054222+tharakadasun@users.noreply.github.com>
2023-03-30 10:45:31 +0530

    Add files via upload

--- src/config/baseUrl.js
+++ src/config/baseUrl.js
@@ -1,3 +1,3 @@
-// const baseURL = "http://bumbelbeeawsrds-env.eba-2pymabif.us-east-1.elasticbeanstalk.com/api/v1";
-const baseURL = "http://localhost:8082/api/v1";
+const baseURL = "bumbelbeeawsrds-env.eba-2pymabif.us-east-1.elasticbeanstalk.com/api/v1";
+// const baseURL = "localhost:8082/api/v1";
 export default baseURL;
